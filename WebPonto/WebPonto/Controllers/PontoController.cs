c67db7348ffb56fa62c740713ee8ea6dff3a57d2
MathPalma <matheus.palma692@gmail.com>
2021-08-18 20:08:58 -0300

    Estrutura de pastas

---
+++ WebPonto/WebPonto/Controllers/PontoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPonto.Controllers
+{
+    public class PontoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
